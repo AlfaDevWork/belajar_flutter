1dc52a18e6ad02405682a6ecf7eabaa692770caf
Alfarezhi <alfarezhi.dev@gmail.com>
2025-05-20 14:39:21 +0700

    baris 2

--- lib/meet_2/meet_2.dart
+++ lib/meet_2/meet_2.dart
@@ -8,24 +8,50 @@ class MeetDua extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Profil Lengkap', style: TextStyle()),
-        backgroundColor: Colors.lightBlue,
+        leading: IconButton(
+          icon: Icon(Icons.menu, color: Colors.white),
+          onPressed: () {},
+        ),
+        title: Text('Profil Lengkap', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.blue,
       ),
-      body:Column(
-          children: [
-            SizedBox(height: 10),
-            Center(
-              child: Text(
-                'Alfarezhi Mohamad Rasidan',
-                style: GoogleFonts.poppins(fontSize: 22),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('assets/images/download.jpeg'),
+            ),
+          ),
+          SizedBox(height: 10),
+          Center(
+            child: Text(
+              'Alfarezhi Mohamad Rasidan',
+              style: GoogleFonts.poppins(fontSize: 22),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              padding: EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                color: Colors.lightBlue,
+                borderRadius: BorderRadius.all(Radius.circular(24)),
+                border: Border.all(),
+              ),
+              child: Row(
+                children: [
+                SizedBox(
+                // height: 10,
+                width: 35, child: Icon(Icons.email, color: Colors.white),),
+                Text('alfarezhi.dev@gmail.com', style: TextStyle(color: Colors.white),),
+                ],
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(),
-            )
-          ],
-        ),
+          ),
+        ],
+      ),
     );
   }
 }
