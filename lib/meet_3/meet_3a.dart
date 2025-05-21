c8b73a53f40b6e87601aa4a8ab007eb269010de7
silvalfare <silvalfare@gmail.com>
2025-05-21 21:51:08 +0700

    ig

--- lib/meet_3/meet_3a.dart
+++ lib/meet_3/meet_3a.dart
@@ -6,51 +6,83 @@ class MeetTigaA extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.blue, title: Text('Meet Tiga A')),
-      body: SingleChildScrollView(
-        physics: ClampingScrollPhysics(),
-        child: Center(
-          child: Column(
-            children: [
-              buildstack(),
-              buildstack(),
-              buildstack(),
-              buildstack(),
-              buildstack(),
-              buildstack(),
-            ],
+      appBar: AppBar(
+        backgroundColor: Colors.red[900],
+        title: Text(
+          'Ren Amamiya',
+          style: TextStyle(
+            fontSize: 24,
+            color: Colors.white,
+            fontWeight: FontWeight.w400,
           ),
         ),
       ),
-    );
-  }
-
-  Stack buildstack() {
-    return Stack(
-      alignment: Alignment.center,
-      children: [
-        Container(
-          height: 200,
-          width: 200,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: Colors.yellow,
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Container(
+                  height: 100,
+                  width: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.blue[200],
+                    borderRadius: BorderRadius.all(Radius.circular(50)),
+                  ),
+                  child: Center(
+                    child: CircleAvatar(
+                      radius: 50,
+                      backgroundImage: AssetImage(
+                        'assets/images/download.jpeg',
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Text('2', style: TextStyle(fontSize: 15)),
+                        Text('posts', style: TextStyle(fontSize: 15)),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: Column(
+                      children: [
+                        Text('26', style: TextStyle(fontSize: 15)),
+                        Text('followers', style: TextStyle(fontSize: 15)),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: Column(
+                      children: [
+                        Text('59', style: TextStyle(fontSize: 15)),
+                        Text('following', style: TextStyle(fontSize: 15)),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
-        ),
-        Container(
-          height: 200,
-          width: 150,
-          decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.red),
-        ),
-        Container(
-          height: 200,
-          width: 100,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: Colors.green,
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
